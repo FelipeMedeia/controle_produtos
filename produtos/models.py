e8f34a923953cb63710ee1265875471d5ebfdb90
FelipeMedeia <felipemedeia560@gmail.com>
2026-01-17 16:26:16 -0300

    ADD: Construção dos Models de Vendas/ e Produtos/

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=150)
+    marca = models.CharField(max_length=100,blank=True,null=True)
+    tamanho = models.CharField(max_length=50,blank=True,null=True)
+
+    preco_normal = models.DecimalField(max_digits=10,decimal_places=2)
+    preco_descontado = models.DecimalField(max_digits=10,decimal_places=2,null=True)
+
+    quantidade_estoque = models.PositiveIntegerField(default=0)
+    data_validade = models.DateField(blank=True,null=True)
+
+    foto = models.ImageField(upload_to="produtos/",blank=True,null=True)
+
+    criado_em = models.DateTimeField(default=timezone.now)
+
+    def _str_(self):
+        return self.nome
+    
+    def preco_avista(self):
+        return self.preco_descontado or self.preco_normal
+    
+    def preco_prazo(self):
+        return self.preco_normal
+    
+    def baixar_estoque(self, quantidade):
+        if quantidade > self.quantidade_estoque:
+            raise ValueError("Estoque insuficiente!")
+        self.quantidade_estoque-=quantidade
+        self.save()
--- vendas/models.py
+++ vendas/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils import timezone
+from produtos.models import Produto
 
-# Create your models here.
+class Venda(models.Model):
+
+    FORMA_PAGAMENTO_CHOICES = (("avista", "À Vista"),("parcelado", "Parcelado"),)
+
+    nome_comprador = models.CharField(max_length=150)
+    data_venda = models.DateTimeField(default=timezone.now)
+    forma_pagamento = models.CharField(max_length=10,choices=FORMA_PAGAMENTO_CHOICES)
+
+    valor_total = models.DecimalField(max_digits=10,decimal_places=2,default=0)
+
+    def __str__(self):
+        return f"Venda #{self.id} - {self.nome_comprador}"
+    
+    def calcular_total(self):
+        total = sum(item.subtotal() for item in self.itens.all())
+        self.valor_total = total
+        self.save()
+        return total
+    
+
+class ItemVenda(models.Model):
+    venda = models.ForeignKey(Venda, related_name="itens",on_delete=models.CASCADE)
+
+    produto = models.ForeignKey(Produto,on_delete=models.PROTECT)
+
+    quantidade = models.PositiveIntegerField()
+    preco_unitario = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.produto.nome}({self.quantidade})"
+    
+    def subtotal(self):
+        return self.quantidade * self.preco_unitario
+    
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            if self.venda.forma_pagamento =="avista":
+                self.preco_unitario = self.produto.preco_avista()
+            else:
+                self.preco_unitario = self.produto.preco_prazo()
+            
+            self.produto.baixar_estoque(self.quantidade)
+        
+        super().save(*args,**kwargs)
+
+        self.venda.calcular_total
